d44188ab10df9a9deba40d0f3018a89fd7c0e292
isaclaleo <isac.renovato@librerialeo.com.mx>
2024-02-19 17:48:03 -0600

    one exercise �

--- To-do-data/Practice-SQL/Hard/hard_practice.sql
+++ To-do-data/Practice-SQL/Hard/hard_practice.sql
@@ -53,3 +53,21 @@ select
   ) as password
 from patients
 join admissions on patients.patient_id = admissions.patient_id
+
+-- Questions: Each admission costs $50 for patients without insurance, and $10 for patients with insurance. All patients with an even patient_id have insurance. Give each patient a 'Yes' if they have insurance, and a 'No' if they don't have insurance. Add up the admission_total cost for each has_insurance group.
+-- Solution:
+SELECT
+  has_insurance,
+  CASE
+    WHEN has_insurance = 'Yes' THEN COUNT(has_insurance) * 10
+    ELSE count(has_insurance) * 50
+  END AS cost_after_insurance
+FROM (
+    SELECT
+      CASE
+        WHEN patient_id % 2 = 0 THEN 'Yes'
+        ELSE 'No'
+      END AS has_insurance
+    FROM admissions
+  )
+GROUP BY has_insurance
