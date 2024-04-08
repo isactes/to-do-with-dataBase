189e0b2a80f463652f43efb86fa9f4d704128cd1
isaclaleo <isac.renovato@librerialeo.com.mx>
2024-04-08 17:19:43 -0600

    SLQ exercise of the days

--- To-do-data/Practice-SQL/BD-Northwind/Medium/medium_practice.sql
+++ To-do-data/Practice-SQL/BD-Northwind/Medium/medium_practice.sql
@@ -13,5 +13,35 @@ JOIN
   categories ON products.category_id = categories.category_id;
 
 
+-- Description: Show the category_name and the average product unit price for each category rounded to 2 decimal places.
+-- Solucioton: 
+SELECT
+  categories.category_name AS category_name,
+  ROUND(AVG(products.unit_price), 2) AS average_unit_price
+FROM categories
+JOIN products ON products.category_id = categories.category_id
+GROUP BY categories.category_name;
+
+
+-- Description: Show the city, company_name, contact_name from the customers and suppliers table merged together. Create a column which contains 'customers' or 'suppliers' depending on the table it came from.
+-- Solucioton:
+SELECT
+  city,
+  company_name,
+  contact_name,
+  'customers' AS source_table
+FROM customers
+UNION
+SELECT
+  city,
+  company_name,
+  contact_name,
+  'suppliers' AS source_table
+FROM suppliers;
+
+-- Description: 
+-- Solucioton:
+-- Description: 
+-- Solucioton:
 -- Description: 
 -- Solucioton:
